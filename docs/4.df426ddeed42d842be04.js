412f7a88e70e94368489b4f59c7c15bb227bf2a6
laulorenz <lautaro.lorenz@gmail.com>
2021-04-02 11:16:47 -0300

    material

--- docs/4.df426ddeed42d842be04.js
+++ docs/4.df426ddeed42d842be04.js
@@ -1,1 +1,64 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"99Un":function(n,t,e){"use strict";e.r(t),e.d(t,"HomeModule",function(){return u});var o=e("ofXK"),r=e("tyNb"),s=e("fXoL");const c=[{path:"",component:(()=>{class n{}return n.\u0275fac=function(t){return new(t||n)},n.\u0275cmp=s.xb({type:n,selectors:[["app-home"]],decls:2,vars:0,template:function(n,t){1&n&&(s.Gb(0,"p"),s.Qb(1,"home works!"),s.Fb())},styles:[""]}),n})()}];let p=(()=>{class n{}return n.\u0275fac=function(t){return new(t||n)},n.\u0275mod=s.Bb({type:n}),n.\u0275inj=s.Ab({imports:[[r.a.forChild(c)],r.a]}),n})(),u=(()=>{class n{}return n.\u0275fac=function(t){return new(t||n)},n.\u0275mod=s.Bb({type:n}),n.\u0275inj=s.Ab({imports:[[o.b,p]]}),n})()}}]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+	[4],
+	{
+		'99Un': function (n, t, e) {
+			'use strict';
+			e.r(t),
+				e.d(t, 'HomeModule', function () {
+					return u;
+				});
+			var o = e('ofXK'),
+				r = e('tyNb'),
+				s = e('fXoL');
+			const c = [
+				{
+					path: '',
+					component: (() => {
+						class n {}
+						return (
+							(n.ɵfac = function (t) {
+								return new (t || n)();
+							}),
+							(n.ɵcmp = s.xb({
+								type: n,
+								selectors: [['app-home']],
+								decls: 2,
+								vars: 0,
+								template: function (n, t) {
+									1 & n &&
+										(s.Gb(0, 'p'),
+										s.Qb(1, 'home works!'),
+										s.Fb());
+								},
+								styles: ['']
+							})),
+							n
+						);
+					})()
+				}
+			];
+			let p = (() => {
+					class n {}
+					return (
+						(n.ɵfac = function (t) {
+							return new (t || n)();
+						}),
+						(n.ɵmod = s.Bb({ type: n })),
+						(n.ɵinj = s.Ab({ imports: [[r.a.forChild(c)], r.a] })),
+						n
+					);
+				})(),
+				u = (() => {
+					class n {}
+					return (
+						(n.ɵfac = function (t) {
+							return new (t || n)();
+						}),
+						(n.ɵmod = s.Bb({ type: n })),
+						(n.ɵinj = s.Ab({ imports: [[o.b, p]] })),
+						n
+					);
+				})();
+		}
+	}
+]);
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material';
+
+@NgModule({
+	declarations: [],
+	imports: [],
+	exports: [MaterialModule],
+	providers: [],
+	bootstrap: []
+})
+export class SharedModule {}
