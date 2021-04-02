e51a59491bdf67ba7b8f9e0281e7dc0272996a15
laulorenz <lautaro.lorenz@gmail.com>
2021-04-02 11:40:41 -0300

    linkedin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+	public redirectToLinkedin(): void {
+		window.open('https://www.linkedin.com/in/lautaro-lorenz-792629195/');
+	}
+}
