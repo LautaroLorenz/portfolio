e5585ba19b7613c79f4eb23d5187a24a428eec5e
laulorenz <lautaro.lorenz@gmail.com>
2021-04-02 00:03:19 -0300

    v6

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# generate publish version
+ng build --prod=true --outputPath=docs --baseHref=/portfolio/
+git add ./docs
+git commit --no-verify -m "build docs"
+git push --no-verify
+
 # merge develop into main
 git checkout main
 git merge develop
