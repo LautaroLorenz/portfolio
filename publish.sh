6dd349755ac4be2a552461fbc71f68851bf63aa5
laulorenz <lautaro.lorenz@gmail.com>
2021-04-02 00:05:50 -0300

    publish

--- publish.sh
+++ publish.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 # generate publish version
+echo "generando... docs"
 ng build --prod=true --outputPath=docs --baseHref=/portfolio/
 
 # upload publish version
+echo "subiendo a git... docs"
 git add ./docs
 git commit --no-verify -m "build docs"
 git push --no-verify
 
 # merge develop into main
+echo "publicando... docs"
 git checkout main
 git merge develop
 git push --no-verify
-
-# return to develop
 git checkout develop
+
+echo "publicado."
+
